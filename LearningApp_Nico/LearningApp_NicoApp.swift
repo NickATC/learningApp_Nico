12aa05a103502afc92ab121d0fd14942ebbf1398
Nicolás Táutiva <33504315+NickATC@users.noreply.github.com>
2022-01-20 11:36:23 -0500

    Initial Commit

---
+++ LearningApp_Nico/LearningApp_NicoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningApp_NicoApp.swift
+//  LearningApp_Nico
+//
+//  Created by NICOLAS  TAUTIVA on 20/01/22.
+//
+
+import SwiftUI
+
+@main
+struct LearningApp_NicoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
